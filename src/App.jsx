9e228c8350a276b265eaae1c19bb979677b9c122
pavan25022005 <pavansaikrishnakotti@gmail.com>
2025-02-10 21:58:50 +0530

    Fix import case sensitivity for Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import About from './components/About.jsx'
 import Features from './components/Features.jsx'
-import Hero from './components/hero.jsx'
+import Hero from './components/Hero.jsx'
 import Navbar from './components/Navbar.jsx'
 import Story from './components/Story.jsx'
 
